49d33be3823198bd5f0063522d6bcea56a6a41b1
lgouvin <gouvin.lucy@gmail.com>
2023-06-25 15:22:52 -0400

    last improvements, added README

--- assets/javascript/leaderboard_script.js
+++ assets/javascript/leaderboard_script.js
@@ -2,20 +2,24 @@
 var leaderboardEl = $(".score-list")
 // Get the leaderboard from local storage
 leaderboard = JSON.parse(localStorage.getItem("leaderboard"))
-
-// Create scoreEntry elements for each object in the leaderboard, append to leaderboardEl
-for (var i = 0; i < leaderboard.length; i++) {
-    var scoreEntry = $("<li>")
-    scoreEntry.text(leaderboard[i].playerInitials + " - " + leaderboard[i].playerScore)
-    leaderboardEl.append(scoreEntry)
+if (leaderboard === null) {
+    leaderboardEl.text("No scores yet!")
+} else {
+    // Create scoreEntry elements for each object in the leaderboard, append to leaderboardEl
+    for (var i = 0; i < leaderboard.length; i++) {
+        var scoreEntry = $("<li>")
+        scoreEntry.text(leaderboard[i].playerInitials + " - " + leaderboard[i].playerScore)
+        leaderboardEl.append(scoreEntry)
+    }
 }
+
 // If you click the reset button, clear the local storage and wipe the page
-$("button[name = 'reset']").on("click", function(){
+$("button[name = 'reset']").on("click", function () {
     localStorage.clear();
     leaderboardEl.empty();
 })
 
 // If you click the return button, go back to index
-$("button[name = 'return']").on("click", function(){
+$("button[name = 'return']").on("click", function () {
     window.location.href = "index.html"
 })
